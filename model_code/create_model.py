8291685b55be7ed26a6dfa2dc365d5752c7539bc
Alexandru Gliga <gligalexandru@gmail.com>
2025-05-24 13:06:00 +0200

    Implemented model development testing

--- model_code/create_model.py
+++ model_code/create_model.py
@@ -4,8 +4,9 @@ from data_preprocessing import get_dataset
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+
 def train_model(X_train, X_test, y_train, y_test):
-    classifier = GaussianNB()
+    classifier = GaussianNB(var_smoothing=2e-9)
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
@@ -18,11 +19,13 @@ def train_model(X_train, X_test, y_train, y_test):
 
     return classifier
 
+
 def save_model(model):
     joblib.dump(model, 'models/naive_bayes.joblib')
 
+
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = get_dataset()
-    
+
     model = train_model(X_train, X_test, y_train, y_test)
     save_model(model)
