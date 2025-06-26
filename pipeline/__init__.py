6fd79fe747697f6c92606ea4b71b728dc3e42e59
Yulin <y.chen-112@student.tudelft.nl>
2025-06-26 18:35:12 +0200

    Add docstring (pylint warning)

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,1 +1,7 @@
+"""
+This package contains the main pipeline scripts for the model-training process.
+
+This __init__.py file makes the 'pipeline' directory a Python package, allowing
+for easier imports of its modules. It also exposes the application version.
+"""
 __version__ = "v2.0.1-pre-20250626015637"
